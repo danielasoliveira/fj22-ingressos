f57198d36a18666cfb8ac301035addbfae357dba
Daniel Oliveira <danielalexandre.pg@gmail.com>
2018-10-15 22:40:57 -0300

    Criacao da listagem de filmes

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -1,7 +1,10 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sessao;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -22,6 +27,9 @@ public class FilmeController {
     @Autowired
     private FilmeDao filmeDao;
 
+    
+    @Autowired
+    private SessaoDao sessaoDao;
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
@@ -71,5 +79,26 @@ public class FilmeController {
     public void delete(@PathVariable("id") Integer id){
         filmeDao.delete(id);
     }
+    
+    
+    @GetMapping("/filme/em-cartaz")
+    public ModelAndView emCartaz()
+    {
+    	ModelAndView mv = new ModelAndView("filme/em-cartaz");
+    	mv.addObject("filmes", filmeDao.findAll());
+    	return mv;
+    }
+    
+    @GetMapping("/filme/{id}/detalhe")
+    public ModelAndView detalhes(@PathVariable("id") Integer id)
+    {
+    	ModelAndView mv = new ModelAndView("/filme/detalhe");
+    	
+    	Filme filme = filmeDao.findOne(id);
+    	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+    	mv.addObject("sessoes", sessoes);
+    	return mv;
+    }
+    
 
 }
